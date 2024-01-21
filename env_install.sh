04081d5a813eba40e9dbeac9d2762009d8ef80e4
simmondobber <wojciechfbaranowski@gmail.com>
2024-01-21 19:48:17 +0100

    automatic geany config directory creation

--- env_install.sh
+++ env_install.sh
@@ -7,33 +7,25 @@ sudo apt-get install -y geany
 #geany config:
 
 USERNAME=$(whoami)
-CONFIG_FILE="/home/$USERNAME/.config/geany/geany.conf"
-KEYBINDINGS_FILE="/home/$USERNAME/.config/geany/keybindings.conf"
-COLOR_SCHEMES_FILE="/home/$USERNAME/.config/geany/colorschemes/bespin.conf"
-FILETYPES_FILE="/home/$USERNAME/.config/geany/filedefs/filetypes.cpp"
-TEMPLATE_FILE="/home/$USERNAME/.config/geany/templates/files/template.cpp"
+CONFIG_DIR="/home/$USERNAME/.config/geany"
 BASHRC_FILE="/home/$USERNAME/.bashrc"
 
-geany &
-GEANY_PID=$!
-sleep 1
-kill -9 $GEANY_PID
+mkdir $CONFIG_DIR
+mkdir $CONFIG_DIR/colorschemes
+mkdir $CONFIG_DIR/filedefs
+mkdir -p $CONFIG_DIR/templates/files
 
-mkdir $(dirname $COLOR_SCHEMES_FILE)
+curl -o "$CONFIG_DIR/geany.conf" "https://raw.githubusercontent.com/Wojciech-Baranowski/GeanyConfig/main/geany.conf"
+curl -o "$CONFIG_DIR/keybindings.conf" "https://raw.githubusercontent.com/Wojciech-Baranowski/GeanyConfig/main/keybindings.conf"
+curl -o "$CONFIG_DIR/colorschemes/bespin.conf" "https://raw.githubusercontent.com/Wojciech-Baranowski/GeanyConfig/main/bespin.conf"
+curl -o "$CONFIG_DIR/filedefs/filetypes.cpp" "https://raw.githubusercontent.com/Wojciech-Baranowski/GeanyConfig/main/filetypes.cpp"
+curl -o "$CONFIG_DIR/templates/files/template.cpp" "https://raw.githubusercontent.com/Wojciech-Baranowski/GeanyConfig/main/template.cpp"
 
-curl -o $CONFIG_FILE "https://raw.githubusercontent.com/Wojciech-Baranowski/GeanyConfig/main/geany.conf"
-curl -o $KEYBINDINGS_FILE "https://raw.githubusercontent.com/Wojciech-Baranowski/GeanyConfig/main/keybindings.conf"
-curl -o $COLOR_SCHEMES_FILE "https://raw.githubusercontent.com/Wojciech-Baranowski/GeanyConfig/main/bespin.conf"
-curl -o $FILETYPES_FILE "https://raw.githubusercontent.com/Wojciech-Baranowski/GeanyConfig/main/filetypes.cpp"
-curl -o $TEMPLATE_FILE "https://raw.githubusercontent.com/Wojciech-Baranowski/GeanyConfig/main/template.cpp"
-
-#shell settings:
+#shell config:
 
 sed -i '/force_color_prompt=/c\force_color_prompt=yes' $BASHRC_FILE
 if ! grep -q "ulimit" $BASHRC_FILE; then
 	echo -e "\nulimit -s 2000311" >> $BASHRC_FILE
 	source $BASHRC_FILE
 fi
-
- 
 
